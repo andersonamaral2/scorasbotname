3aba4820717fe077cdfea2019f14e9399215752c
Anderson L Amaral <72057917+andersonamaral2@users.noreply.github.com>
2024-04-14 18:55:05 -0300

    Add files via upload
    "Add scorasbot.js to log 'Scorasbot' on page load"

---
+++ scorasbot.js
@@ -0,0 +1,6 @@
+// scorasbot.js
+// This script logs 'Scorasbot' to the console when the web page loads
+
+document.addEventListener('DOMContentLoaded', function() {
+    console.log('Scorasbot');
+});
